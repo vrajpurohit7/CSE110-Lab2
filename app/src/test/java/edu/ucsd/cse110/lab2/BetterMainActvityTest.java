6f5292055c921430c2294207b8c803dddd5cf4c7
vrajpurohit7 <vrajpurohit@ucsd.edu>
2023-01-18 17:16:48 -0800

    add missing changes

--- app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActvityTest.java
+++ app/src/test/java/edu/ucsd/cse110/lab2/BetterMainActvityTest.java
@@ -1,2 +1,33 @@
-package edu.ucsd.cse110.lab2;public class BetterMainActvityTest {
+package edu.ucsd.cse110.lab2;
+
+import static org.junit.Assert.assertEquals;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class BetterMainActvityTest {
+    @Test
+    public void test_one_plus_one_equals_two(){
+        var scenario = ActivityScenario.launch(MainActivity.class);
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.STARTED);
+
+        scenario.onActivity(activity -> {
+            activity.findViewById(R.id.btn_one).performClick();
+            activity.findViewById(R.id.btn_plus).performClick();
+            activity.findViewById(R.id.btn_one).performClick();
+            activity.findViewById(R.id.btn_equals).performClick();
+            activity.findViewById(R.id.btn_two).performClick();
+            TextView answer = activity.findViewById(R.id.display);
+            assertEquals("2", answer.getText());
+        });
+    }
 }
--- app/src/test/java/edu/ucsd/cse110/lab2/UtilitiesTest.java
+++ app/src/test/java/edu/ucsd/cse110/lab2/UtilitiesTest.java
@@ -1,2 +1,13 @@
-package edu.ucsd.cse110.lab2;public class UtilitiesTest {
+package edu.ucsd.cse110.lab2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class UtilitiesTest {
+    @Test
+    public void test_trimDisplayStr(){
+        assertEquals("1.2340", Utilities.trimDisplayStr("1.2340"));
+        assertEquals("13", Utilities.trimDisplayStr("13.0"));
+    }
 }
